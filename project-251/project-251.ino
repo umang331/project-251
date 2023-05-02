33b3f547a15e222b6e6c558bdde88ce607535b31
Umang balicha <54236975+umang331@users.noreply.github.com>
2023-05-02 13:43:38 +0530

    Add files via upload

---
+++ project-251/project-251.ino
@@ -0,0 +1,25 @@
+#include<WiFi.h>
+
+const char ssid[] = "ssid";
+const char password[] = "password";
+const int potpin = 34;
+
+void setup(){
+  Serial.begin(115200);
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid , password);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println("Connected with Wifi");
+}
+
+void loop(){
+  int pot = analogRead(potpin);
+  Serial.print("Pot values: ");
+  Serial.println(pot);
+  delay(1000);
+}
